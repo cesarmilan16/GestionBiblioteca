725e47e48df5809b43883e49f8bb425f875d2a4b
cesarmilan16 <cesar3bfermi@gmail.com>
2024-04-29 19:51:22 +0200

    mas cambios

--- src/Modelo/Libro.java
+++ src/Modelo/Libro.java
@@ -16,16 +16,8 @@ public class Libro {
         return titulo;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
     public String getCategoria() {
         return categoria;
-    }
-
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
     }
 
     public boolean isDisponible() {
